53d5dd77ac69dda54a75a8ad457410517608efb0
Jun Kiat <junkiatang@gmail.com>
2022-05-08 18:33:19 +0800

    match for succesful match

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-const testFunction = () => {
-	console.log("hi");
+let testArr = [];
+
+const winArr = [];
+
+const testIfMatch = () => {
+	const $firstItem = testArr[0];
+	const $secondItem = testArr[1];
+
+	if ($firstItem.attr("data-img") === $secondItem.attr("data-img")) {
+		console.log("hihi");
+		// push matching items to win Array
+		winArr.push($firstItem);
+		winArr.push($secondItem);
+		// clear out the testArr
+		testArr = [];
+		console.log(testArr);
+		console.log(winArr);
+	}
+};
+
+const testFunction = (event) => {
+	if (testArr.length === 0) {
+		const clicked = $(event.currentTarget).css("opacity", 1);
+		testArr.push(clicked);
+	} else if (testArr.length === 1) {
+		const clicked = $(event.currentTarget).css("opacity", 1);
+		testArr.push(clicked);
+		testIfMatch();
+	}
 };
 
 $(() => {
